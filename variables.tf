1de8e5c8520e1de9d52b496e07ed651b3cb4c21e
ramosops <ramosdevops@gmail.com>
2022-12-30 15:41:24 -0300

    update files

--- variables.tf
+++ variables.tf
@@ -1,8 +1,5 @@
 variable "region" {}
 variable "environment" {}
 variable "billing" {}
-variable "asg_desired" {}
-variable "asg_min_size" {}
-variable "asg_max_size" {}
 variable "service_config" {}
 variable "certificate_ssl" {}
